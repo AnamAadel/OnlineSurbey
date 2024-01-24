ec88146c624ae71bc627f7f29b6a260a9e993eaf
mihir-x <ictlancerx@gmail.com>
2024-01-24 18:20:38 +0600

    [page](faq) added faq page banner

---
+++ src/app/primary/(primary)/faq/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Faqbanner from "./FaqBanner";
+
+const faq = () => {
+    return (
+        <div>
+            <Faqbanner></Faqbanner>
+        </div>
+    );
+};
+
+export default faq;
